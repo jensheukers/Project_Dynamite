e2e9e245ded2b9a1d0c1e09d3fd76ab2f1fde640
Jens Heukers <jens.heukers@hotmail.com>
2018-11-08 23:07:21 +0100

    Revert to preset

--- game/game.cpp
+++ game/game.cpp
@@ -7,20 +7,9 @@
 */
 
 #include "../dynamite/core.h"
-#include "../dynamite/ui/text.h"
 #include "game.h"
 
 Game::Game() {
-	SceneManager::Instance()->LoadExternalScene(Core::Instance()->GetResourcePath("test.dynamite"));
-	SceneManager::Instance()->SetActiveScene("test");
-
-	Font* bluefont = new Font(Core::Instance()->GetResourcePath("font2.tga"), Core::Instance()->GetResourcePath("font2.csv"));
-	bluefont->SetColor(ColorRGB(0,0,255));
-
-	Text* text = new Text(bluefont);
-	text->SetText("Bigger Text");
-	text->position = Vector2(50,50);
-	SceneManager::Instance()->GetActiveScene()->AddUIElement(text);
 }
 
 void Game::Update() {	
